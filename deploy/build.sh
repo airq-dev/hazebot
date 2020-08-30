5a6d358d4ff9ad689cce0668d226d2c305fad7fb
Ian Hoffman <ianhoffman10@gmail.com>
2020-08-30 15:23:05 -0700

    wip

--- deploy/build.sh
+++
@@ -1,6 +0,0 @@
-ECR_ROOT="146486422038.dkr.ecr.us-west-1.amazonaws.com"
-
-cd ..
-docker-compose -f docker-compose.prod.yml build
-aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_ROOT
-docker-compose -f docker-compose.prod.yml push
--- deploy/update-services.sh
+++
@@ -1,8 +0,0 @@
-aws ecs update-service \
-    --service app \
-    --cluster airq \
-    --region us-west-1 \
-    --force-new-deployment \
-    --desired-count 2 \
-    --deployment-configuration minimumHealthyPercent=50,maximumPercent=100 \
-    --profile airq
