632e4fb4708aa07094c4317aa8c9c4c391f7b61a
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-05 21:41:22 -0700

    Add profiling

--- app/airq/middleware.py
+++ app/airq/middleware.py
@@ -1,6 +1,13 @@
 import json
 import logging
 import time
+import urllib
+
+from werkzeug.middleware import profiler
+
+#
+# TODO: Figure out how to add typing to this file.
+#
 
 
 logger = logging.getLogger(__name__)
@@ -32,3 +39,19 @@ class LoggingMiddleware:
             return start_response(status, headers, exc_info)
 
         return self.app(environ, _start_response)
+
+
+class ProfilerMiddleware(profiler.ProfilerMiddleware):
+    """A version of Werkzeug's ProfilerMiddleware which only
+    profiles a request when profile=1 is set in the query string;
+    e.g., localhost:5000/quality?zipcode=94703&profile=1
+    """
+
+    def __call__(self, environ, start_response):
+        query_string = environ.get("QUERY_STRING", "")
+        args = urllib.parse.parse_qs(query_string)
+        if args.get("profile"):
+            # Profile the response.
+            return super().__call__(environ, start_response)
+        # Just do the default behavior; no profiling.
+        return self._app(environ, start_response)
