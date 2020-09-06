d1465d41323af831e51e57e706404f9ed55c1944
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-06 13:20:45 -0700

    get remote ip addr

--- app/airq/__init__.py
+++ app/airq/__init__.py
@@ -30,7 +30,8 @@ def sms_reply() -> str:
 def quality() -> str:
     zipcode = request.args.get("zipcode", "").strip()
     message = _get_message_for_zipcode(zipcode, separator="<br>")
-    db.insert_request(zipcode, request.remote_addr, db.ClientIdentifierType.IP)
+    ip_addr = request.headers.get("X-Forwarded-For", request.remote_addr)
+    db.insert_request(zipcode, ip_addr, db.ClientIdentifierType.IP)
     return message
 
 
