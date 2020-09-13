8b3bcf928e698ee386b4f1c8e6e27d3a51a929b5
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-13 14:56:30 -0700

    Query metrics table

--- app/airq/config.py
+++ app/airq/config.py
@@ -44,7 +44,8 @@ app = flask.Flask(__name__)
 # We have to use this `setattr` hack here or Mypy gets really confused.
 # See https://github.com/python/mypy/issues/2427 for details.
 setattr(app, "wsgi_app", middleware.LoggingMiddleware(app.wsgi_app))
-setattr(app, "wsgi_app", middleware.ProfilerMiddleware(app.wsgi_app, restrictions=[30]))
+import pstats
+setattr(app, "wsgi_app", middleware.ProfilerMiddleware(app.wsgi_app, restrictions=[30], sort_by=('cumtime', 'tottime')))
 
 config = {
     "SQLALCHEMY_DATABASE_URI": f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}",
