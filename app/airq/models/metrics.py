562c24bae000eebf66e4b02ac22d50e98d672e4e
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-13 13:35:45 -0700

    Aggregate historical data (#78)
    * Aggregate historical data
    
    * wip

---
+++ app/airq/models/metrics.py
@@ -0,0 +1,20 @@
+from airq.config import db
+
+
+class Metric(db.Model):  # type: ignore
+    __tablename__ = "metrics"
+
+    zipcode_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("zipcodes.id", name="metrics_zipcodes_fkey"),
+        primary_key=True,
+        nullable=False,
+    )
+    timestamp = db.Column(db.Integer(), primary_key=True, nullable=False)
+    value = db.Column(db.Float(), nullable=False)
+    num_sensors = db.Column(db.Integer(), nullable=False)
+    max_sensor_distance = db.Column(db.Float(), nullable=False)
+    min_sensor_distance = db.Column(db.Float(), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Metric {self.value} from {self.num_sensors}>"
