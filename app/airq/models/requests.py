d6c5f2a2f3d3ef29c17c11d3366f1303812c6858
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-14 14:04:33 -0700

    Requests refactor cleanup (#90)
    * Requests refactor cleanup
    
    * wip

--- app/airq/models/requests.py
+++ app/airq/models/requests.py
@@ -4,24 +4,24 @@ from airq.config import db
 class Request(db.Model):  # type: ignore
     __tablename__ = "requests"
 
-    id = db.Column(db.Integer, primary_key=True)
-    zipcode = db.Column(db.String(5), index=True, nullable=False)
     zipcode_id = db.Column(
         db.Integer(),
         db.ForeignKey("zipcodes.id", name="requests_zipcode_id_fkey"),
-        nullable=True,
+        nullable=False,
+        primary_key=True,
     )
     client_id = db.Column(
         db.Integer(),
         db.ForeignKey("clients.id", name="requests_client_id_fkey"),
         nullable=False,
+        primary_key=True,
     )
     count = db.Column(db.Integer, nullable=False)
     first_ts = db.Column(db.Integer, nullable=False)
     last_ts = db.Column(db.Integer, nullable=False)
 
     client = db.relationship("Client")
-    # zipcode = db.relationship('Zipcode', back_populates='requests')
+    zipcode = db.relationship("Zipcode")
 
     def __repr__(self) -> str:
-        return f"<Request {self.zipcode}>"
+        return f"<Request {self.client_id} - {self.zipcode_id}>"
