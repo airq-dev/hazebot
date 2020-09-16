7fa72fcf1ecbd6390e5182d92ddef83dffeedd31
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-16 13:04:29 -0700

    Increase estimated AQI accuracy by narrowing radius (#122)
    * Increase estimated AQI accuracy by narrowing radius
    
    * allow fewer readings as well
    
    * wip

--- app/airq/sync/purpleair.py
+++ app/airq/sync/purpleair.py
@@ -23,11 +23,11 @@ TRelationsMap = typing.Dict[int, typing.Dict[int, float]]
 
 PURPLEAIR_URL = "https://www.purpleair.com/json"
 
-# Try to get at least 10 readings per zipcode.
-DESIRED_NUM_READINGS = 10
+# Try to get at least 8 readings per zipcode.
+DESIRED_NUM_READINGS = 8
 
-# Allow any number of readings within 5km from the zipcode centroid.
-DESIRED_READING_DISTANCE_KM = 5
+# Allow any number of readings within 2.5km from the zipcode centroid.
+DESIRED_READING_DISTANCE_KM = 2.5
 
 
 def _get_purpleair_data() -> typing.List[typing.Dict[str, typing.Any]]:
