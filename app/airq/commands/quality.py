ff99fd97ea9813088755c1029d478f1fa20f3f51
Ian Hoffman <ianhoffman10@gmail.com>
2020-09-16 23:08:54 -0700

    copy changes (#130)
    * copy changes
    
    * segment
    
    * wip

--- app/airq/commands/quality.py
+++ app/airq/commands/quality.py
@@ -70,13 +70,13 @@ class GetDetailsHandler(BaseQualityHandler):
         num_desired = 3
         recommended_zipcodes = zipcode.get_recommendations(num_desired)
         if recommended_zipcodes:
-            message.append("Try these other places near you for better air quality:")
+            message.append("Here are the closest places with better air quality:")
             for recommendation in recommended_zipcodes:
                 message.append(
                     " - {} {}: {}".format(
                         recommendation.city.name,
                         recommendation.zipcode,
-                        recommendation.pm25_level.display,
+                        recommendation.pm25_level.display.upper(),
                     )
                 )
             message.append("")
