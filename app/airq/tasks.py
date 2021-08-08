58c7616b0d31207c9dd7b66b9d4389b91a7a2ae1
Ian Hoffman <ianhoffman10@gmail.com>
2021-08-08 14:48:12 -0700

    allow bulk sending on a per-locale basis

--- app/airq/tasks.py
+++ app/airq/tasks.py
@@ -14,15 +14,16 @@ def models_sync():
 
 
 @celery.task()
-def bulk_send(message: str, last_active_at: float):
+def bulk_send(message: str, last_active_at: float, locale: str):
     from airq.models.clients import Client
 
     num_sent = 0
     for client in Client.query.filter_inactive_since(last_active_at).all():
-        try:
-            if client.send_message(message):
-                num_sent += 1
-        except Exception as e:
-            logger.exception("Failed to send message to %s: %s", client, e)
+        if client.locale == locale:
+            try:
+                if client.send_message(message):
+                    num_sent += 1
+            except Exception as e:
+                logger.exception("Failed to send message to %s: %s", client, e)
 
     logger.info("Sent %s messages", num_sent)
